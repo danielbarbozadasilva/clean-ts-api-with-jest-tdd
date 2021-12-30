212bf6246e06127c41d94419f4c05984c366d8f3
Daniel Barboza da Silva <daniel80barboza@gmail.com>
2021-12-30 19:26:02 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,3 +1,4 @@
+
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
@@ -9,6 +10,6 @@ export const makeSignUpController = (): SignUpController => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  const dDAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dDAddAccount)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
 }
